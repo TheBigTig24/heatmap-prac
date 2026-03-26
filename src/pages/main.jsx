91a5eca33366fff78c364e8fcccc9f1cc05c3923
Caleb C <kaichun24@gmail.com>
2026-03-25 19:09:31 -0700

    first commit

---
+++ src/pages/main.jsx
@@ -0,0 +1,75 @@
+import { useRef, useState, useEffect } from "react";
+import Canvas from "../components/canvas";
+import "../styles/main.css";
+
+const Main = () => {
+
+    const canvasRemoteControl = useRef();
+    const heatmapInstance = useRef(null);
+    const heatmapContainerRef = useRef(null);
+
+    useEffect(() => {
+        if (window.h337) {
+            heatmapInstance.current = h337.create({
+                container: heatmapContainerRef.current,
+                radius: 30,
+                maxOpacity: 0.6,
+                minOpacity: 0,
+                blur: 0.75,
+                max: 20,
+            })
+        }
+    }, []);
+
+    const handleClearReq = () => {
+        if (canvasRemoteControl.current) {
+            canvasRemoteControl.current.clearCanvas();
+        }
+        if (heatmapInstance.current) {
+            heatmapInstance.current.setData({
+                max: 0,
+                data: []
+            });
+        }
+    }
+
+    const updateHeatmap = () => {
+        /** @type {Array} */
+        const data = getHeatmapData();
+        console.log(data);
+        data.forEach((element) => {
+            if (heatmapInstance.current) {
+                heatmapInstance.current.addData({
+                    x: element.x,
+                    y: element.y,
+                    value: 1
+                });
+            }
+        });
+    }
+
+    const getHeatmapData = () => {
+        if (canvasRemoteControl.current) {
+            return canvasRemoteControl.current.getHeatmapData();
+        }
+        return [];
+    }
+
+    return (<>
+        <div className="main-cont">
+            <h1>Google QuickDraw Clone</h1>
+            <div className="interactables-cont">
+                <Canvas ref={canvasRemoteControl}/>
+                <div ref={heatmapContainerRef} className="heatmap">
+
+                </div>
+            </div>
+            <div className="button-cont">
+                <button onClick={handleClearReq}>Clear<i className="fa-solid fa-eraser"></i></button>
+                <button onClick={updateHeatmap}>Update Heatmap<i className="fa-solid fa-fire"></i></button>
+            </div>
+        </div>
+    </>)
+};
+
+export default Main;
